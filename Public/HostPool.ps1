0824945af4f57f9b4dfd649ff70fc05685386a93
Nick Wagner <nwagner@getnerdio.com>
2023-10-26 12:50:10 -0500

    manual changes

--- Public/HostPool.ps1
+++ Public/HostPool.ps1
@@ -193,11 +193,16 @@ Function Get-NmeHostPoolAutoScaleConfig {
 	Set-NmeAuthHeaders
 	Try {
 		$Result = Invoke-RestMethod "$script:NmeUri/api/v1/arm/hostpool/$SubscriptionId/$ResourceGroup/$HostPoolName/auto-scale$Querystring" -Method get -Headers $script:AuthHeaders -ContentType 'application/json'
-		$Result.PSObject.TypeNames.Insert(0, 'NmeDynamicPoolConfiguration')
 		$Result | Add-Member -NotePropertyName 'subscriptionId' -NotePropertyValue $subscriptionId -erroraction 'SilentlyContinue'
 		$Result | Add-Member -NotePropertyName 'resourceGroup' -NotePropertyValue $resourceGroup -erroraction 'SilentlyContinue'
 		$Result | Add-Member -NotePropertyName 'hostPoolName' -NotePropertyValue $hostPoolName -erroraction 'SilentlyContinue'
-		$Result | CapProps
+		$result = $Result | CapProps
+		$Result.AutoScaleTriggers = @($result.autoScaleTriggers)
+		if ($Result.RollingDrainMode.Windows -eq $null) {
+			$result = ($result | select -Property *  -ExcludeProperty RollingDrainMode)
+		}
+		$Result.PSObject.TypeNames.Insert(0, 'NmeDynamicPoolConfiguration')
+		$result
 	}
 	Catch {
 		$message = ParseErrorForResponseBody($_)
@@ -356,7 +361,7 @@ Function Set-NmeHostPoolAutoScaleConfig {
 		[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)][string]$HostPoolName,
 		[Parameter(ValueFromPipelineByPropertyName=$true)][boolean]$MultiTriggers,
 		[Parameter(ValueFromPipelineByPropertyName=$true)][boolean]$AutoHealActions,
-		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$True)][ValidateScript({if ($_.PSObject.TypeNames -contains "NmeDynamicPoolConfiguration"){$true} else{throw " is not a NmeDynamicPoolConfiguration object."}})]$NmeDynamicPoolConfiguration
+		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$True)][ValidateScript({if ($_.PSObject.TypeNames -contains "NmeDynamicPoolConfiguration"){$true} else{throw " is not a NmeDynamicPoolConfiguration object."}})][psobject[]]$NmeDynamicPoolConfiguration
 	)
 	Set-NmeAuthHeaders
 	Try {
